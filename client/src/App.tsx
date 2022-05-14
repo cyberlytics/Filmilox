64daa1e08f3d74ad1e0c9bb4ecd11a3a71aa150d
Eni Veshi <e.veshi@oth-aw.de>
2022-05-14 19:18:03 +0200

    template

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import Counter from './components/Counter';
+
+function App() {
+    return (
+        <div>
+            <div className="bg-sky-200 p-8 shadow">
+                <p className="text-center text-4xl font-bold">Filmilox</p>
+            </div>
+            <Counter />
+        </div>
+    );
+}
+
+export default App;
---
+++ client/src/api/Backend.ts
@@ -0,0 +1,5 @@
+export default class Backend {
+    static fetchData = async () => {
+        return [];
+    };
+}
