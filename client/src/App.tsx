8d96b44a446ebcde0dfb04b88832f8393de7b8c4
Kraus, Kevin <k.kraus1@oth-aw.de>
2022-05-19 23:38:20 +0200

    MovieCards Static Template

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import Counter from './components/Counter';
+import MovieCard from './components/MovieCard';
 
 function App() {
     return (
@@ -6,7 +6,7 @@ function App() {
             <div className="bg-sky-200 p-8 shadow">
                 <p className="text-center text-4xl font-bold">Filmilox</p>
             </div>
-            <Counter />
+            <MovieCard />
         </div>
     );
 }
