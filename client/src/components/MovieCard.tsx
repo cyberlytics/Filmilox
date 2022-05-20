977910c7740eec573af7caf9143aaedef1dc61b8
Kevin Kraus <k.kraus1@oth-aw.de>
2022-05-20 12:11:54 +0200

    MovieCards static View

--- client/src/components/MovieCard.tsx
+++ client/src/components/MovieCard.tsx
@@ -1,31 +1,39 @@
 import Rating from '@mui/material/Rating';
 import { Card, CardContent, CardMedia, Typography } from '@mui/material';
+import React from 'react';
 
-const MovieCard = () => {
+const MovieCard = (props: any) => {
     return (
-        <div className="m-2">
-            <Card sx={{ maxWidth: 200, maxHeight: 380 }}>
-                <CardMedia
-                    component="img"
-                    height="300"
-                    image="https://quotecatalog.imgix.net/assets/titles-CvPsAP4zNZwf2s4VoEdvj6Jl/original.jpg?w=400&h=600&fit=crop"
-                    alt="GoodFellas Movie Poster"
-                ></CardMedia>
-                <CardContent className="content-center">
-                    <Typography component="div" style={{ left: 0 }}>
-                        GoodFellas
-                    </Typography>
-                    <Typography style={{ left: 0 }}>
-                        <Rating
-                            name="movie-rating"
-                            defaultValue={0}
-                            max={10}
-                            size="small"
-                        />
-                    </Typography>
-                </CardContent>
-            </Card>
-        </div>
+        <>
+            {props.movies.map((movie: any, index: any) => (
+                <div className="m-3">
+                    <Card sx={{ maxWidth: 200, maxHeight: 380 }}>
+                        <CardMedia
+                            component="img"
+                            height="300"
+                            src={movie.Poster}
+                            alt="Movie Poster"
+                        ></CardMedia>
+                        <CardContent className="content-center">
+                            <div>
+                                <p className="text-sm">{movie.Title}</p>
+                            </div>
+                            <div
+                                style={{ display: 'flex' }}
+                                className="content-center"
+                            >
+                                <Rating
+                                    name="movie-rating"
+                                    defaultValue={0}
+                                    max={10}
+                                    size="small"
+                                />
+                            </div>
+                        </CardContent>
+                    </Card>
+                </div>
+            ))}
+        </>
     );
 };
 
