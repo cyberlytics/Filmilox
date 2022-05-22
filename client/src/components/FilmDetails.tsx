f2617b4bb7077654ee0bba94361a5988c03762de
Bodner, Manuel <m.bodner@oth-aw.de>
2022-05-22 12:15:40 +0200

    adding a Button to Filmdetails to open the AddReview component

--- client/src/components/FilmDetails.tsx
+++ client/src/components/FilmDetails.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
-import {useParams} from "react-router-dom";
+import { Button } from '@mui/material';
+import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { AddReview } from './AddReview';
 
 function FilmDetails() {
     const params = useParams();
+    const [openAddReview, setOpenAddReview] = useState<boolean>(false);
+
+    const handleAddReviewClick = () => {
+        setOpenAddReview(true);
+    };
+
+    const handleAddReviewClose = () => {
+        setOpenAddReview(false);
+    };
+
     return (
         <div>
-            <p>This should be the Film Details Page for the Film with id {params.filmId}</p>
+            <p>
+                This should be the Film Details Page for the Film with id{' '}
+                {params.filmId}
+            </p>
+            <Button variant="contained" onClick={handleAddReviewClick}>
+                Film bewerten
+            </Button>
+            {openAddReview && params.filmId && (
+                <AddReview
+                    open={openAddReview}
+                    onClose={handleAddReviewClose}
+                    movieId={params.filmId}
+                />
+            )}
         </div>
     );
 }
