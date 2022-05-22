f78201457660eb4c8efb776b2f5f7a47496b6797
Antskaitis Arina <a.antskaitis@oth-aw.de>
2022-05-22 12:21:10 +0000

    Added Comment component file and reference to the FilmDetails page

--- client/src/components/FilmDetails.tsx
+++ client/src/components/FilmDetails.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {useParams} from "react-router-dom";
-
+import Comment from './Comment';
 function FilmDetails() {
     const params = useParams();
     return (
         <div>
             <p>This should be the Film Details Page for the Film with id {params.filmId}</p>
+            <Comment />
         </div>
     );
 }
