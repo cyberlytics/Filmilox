a970502c51e26251b116a76da036d3194654a45d
Bodner, Manuel <m.bodner@oth-aw.de>
2022-05-23 10:50:46 +0200

    fix review and vote schema, change file naming

--- sys-src/server/models/vote.js
+++ sys-src/server/models/vote.js
@@ -1,33 +1,29 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const voteSchema = new mongoose.Schema(
-	{
-		user: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "User",
-			required: true,
-		},
-	},
-	{
-		review: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "Review",
-			required: true,
-		},
-	},
-	{
-		// True  -> Upvote
-		// False -> Downvote
-		state: {
-			type: Boolean,
-			required: true,
-		},
-	},
-	{
-		timestamps: true,
-	}
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        review: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Review',
+            required: true,
+        },
+        // True  -> Upvote
+        // False -> Downvote
+        state: {
+            type: Boolean,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
 );
 
-const Vote = mongoose.model("Vote", voteSchema);
+const Vote = mongoose.model('Vote', voteSchema);
 
 module.exports = Vote;
