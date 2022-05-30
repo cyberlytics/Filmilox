21b2067f0b8d641c2f7aa18b695e875b88d06f6b
Arsen Oganisyan <a.oganisyan@oth-aw.de>
2022-05-31 00:09:25 +0200

    changed frontend for image upload, imported multer
    Former-commit-id: 063b8b0e4d8ac02cb54959c9663c0ea373798751

--- sys-src/client/src/components/Admin.tsx
+++ sys-src/client/src/components/Admin.tsx
@@ -118,12 +118,22 @@ const Admin = () => {
         if (formValid) {
             try {
                 setLoading(true);
-                const status = await Backend.addMovie({
-                    title: title,
-                    description: description,
-                    release: releaseDate,
-                    trailer: trailerLink,
-                });
+                var formData = new FormData();
+
+                formData.append('image', image as Blob);
+                formData.append('title', title);
+                formData.append('description', description);
+                let relDate =
+                    releaseDate?.getDay() +
+                    '.' +
+                    releaseDate?.getMonth() +
+                    '.' +
+                    releaseDate?.getFullYear();
+
+                formData.append('releaseDate', relDate);
+                formData.append('trailer', trailerLink);
+
+                const status = await Backend.addMovie(formData);
                 setLoading(false);
 
                 if (status) {
@@ -225,7 +235,7 @@ const Admin = () => {
                         Bild hinzufügen
                     </Button>
                 </label>
-                <img src={preview} className="w-[300px]" alt=""/>
+                <img src={preview} className="w-[300px]" alt="" />
 
                 <div
                     className="w-[115%]"
