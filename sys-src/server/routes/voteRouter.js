5b12c827b1a34292b8f66490a75594bad1fe573d
Benedikt Niedernhuber <b.niedernhuber@oth-aw.de>
2022-06-03 17:41:40 +0200

    try/catch wraps for voting routes.
    Former-commit-id: 76a1184e7241aef50f466c0f97e6815262b2cb23

--- sys-src/server/routes/voteRouter.js
+++ sys-src/server/routes/voteRouter.js
@@ -10,74 +10,94 @@ router.post(
     body('reviewId').exists().isString(),
     body('isUpvote').exists().isBoolean(),
     async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-        const { reviewId, isUpvote } = req.body;
-        const userId = req.user;
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({errors: errors.array()});
+            }
+            const {reviewId, isUpvote} = req.body;
+            const userId = req.user;
 
-        //Make sure the Review specified actually exists
-        const existingReview = await Review.findById(reviewId);
-        if (!existingReview)
-            return res.status(404).json({
-                errors: [{message: "The review doesn't exist."}],
-            });
-
-        const existingVote = await Vote.findOne({
-            user: userId,
-            review: reviewId,
-        });
-        if (existingVote) {
-            Vote.updateOne({user: userId, review: reviewId}, {state: isUpvote}, (err) => {
-                if (err) {
-                    console.log(err);
-                }
-                return res.status(200).json({
-                    message: 'Vote updated successfully.',
+            //Make sure the Review specified actually exists
+            const existingReview = await Review.findById(reviewId);
+            if (!existingReview)
+                return res.status(404).json({
+                    errors: [{message: "The review doesn't exist."}],
                 });
-            })
 
-        } else {
-            const newVote = new Vote({
+            const existingVote = await Vote.findOne({
                 user: userId,
                 review: reviewId,
-                state: isUpvote,
             });
-            await newVote.save();
+            if (existingVote) {
+                Vote.updateOne({user: userId, review: reviewId}, {state: isUpvote}, (err) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    return res.status(200).json({
+                        message: 'Vote updated successfully.',
+                    });
+                })
 
-            return res.status(200).json({
-                message: 'Vote added successfully.',
-            });
+            } else {
+                const newVote = new Vote({
+                    user: userId,
+                    review: reviewId,
+                    state: isUpvote,
+                });
+                await newVote.save();
+
+                return res.status(200).json({
+                    message: 'Vote added successfully.',
+                });
+            }
+        } catch (e) {
+            return res
+                .status(500)
+                .json({errors: [{message: e.message}]});
         }
+
     }
 );
 
 router.get(
     '/:reviewId',
     async (req, res) => {
-        const { reviewId } = req.params;
-        const existingReview = await Review.findById(reviewId);
-        if (!existingReview)
-            return res.status(404).json({
-                errors: [{message: "The review doesn't exist."}],
-            });
-        const upvotes = await Vote.find({review: reviewId, state: true}).count();
-        const downvotes = await Vote.find({review: reviewId, state: false}).count();
+        try {
+            const {reviewId} = req.params;
+            const existingReview = await Review.findById(reviewId);
+            if (!existingReview)
+                return res.status(404).json({
+                    errors: [{message: "The review doesn't exist."}],
+                });
+            const upvotes = await Vote.find({review: reviewId, state: true}).count();
+            const downvotes = await Vote.find({review: reviewId, state: false}).count();
 
-        return res.json( {upvote: upvotes, downvote: downvotes});
+            return res.json({upvote: upvotes, downvote: downvotes});
+        } catch (e) {
+            return res
+                .status(500)
+                .json({errors: [{message: e.message}]});
+        }
     }
 )
 
 router.get('/existingVote/:reviewId/', auth, async (req, res) => {
-    const {reviewId} = req.params;
-    Vote.findOne({user: req.user, review: reviewId}, (err, vote) => {
-        if (err) {
-            console.log(err);
-        }
-        return res.json({
-             userVote : vote.state
-        });
-    })
+    try {
+        const {reviewId} = req.params;
+        Vote.findOne({user: req.user, review: reviewId}, (err, vote) => {
+            if (err) {
+                console.log(err);
+            }
+            return res.json({
+                userVote: vote.state
+            });
+        })
+    } catch (e) {
+        return res
+            .status(500)
+            .json({errors: [{message: e.message}]});
+    }
+
 })
 module.exports = router;
