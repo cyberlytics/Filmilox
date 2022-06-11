d03df65522bac404f1595a41bc6fa91a88d919b3
Kevin Kraus <k.kraus1@oth-aw.de>
2022-06-11 10:45:14 +0200

    updated Frontend
    Former-commit-id: 06cdcc4ed62aaa5c526f9866b7706e3fc48aea7d

--- sys-src/client/src/components/UserSettings.tsx
+++ sys-src/client/src/components/UserSettings.tsx
@@ -1,11 +1,11 @@
 import {Avatar, Button, Input} from "@mui/material";
-import Controller from "../controller/Controller";
-import IconButton from "@mui/material/IconButton";
 import React, {ChangeEvent, useEffect, useState} from "react";
 import {useAppSelector} from "../redux/hooks";
 import {selectEmail, selectUsername} from "../redux/userSlice";
-import InsertPhotoIcon from "@mui/icons-material/InsertPhoto";
 import styled from "@emotion/styled";
+import Backend from "../api/Backend";
+import SaveIcon from '@mui/icons-material/Save';
+import InsertPhotoIcon from "@mui/icons-material/InsertPhoto";
 
 const UserSettings = () => {
     const username = useAppSelector(selectUsername);
@@ -24,6 +24,13 @@ const UserSettings = () => {
         }
     }
 
+    const handleUpdateProfile = async() =>{
+        const status = await Backend.updateProfile();
+        if (status){
+            setProfilePic(undefined);
+        }
+    }
+
     useEffect(() => {
         if (profilePic) {
             const reader = new FileReader();
@@ -37,33 +44,30 @@ const UserSettings = () => {
     }, [profilePic]);
 
     return (
-        <div className="m-20">
-                <Avatar style={{height: 80, width: 80}}
-                    src={profilePicPreview}
-                    alt={`...Controller.stringAvatar(username)`}
+        <div className="flex flex-col justify-center items-center">
+            <h1 className="font-bold text-4xl mt-8">BENUTZER EINSTELLUNGEN</h1>
+            <label htmlFor="contained-button-file"
+                   className="mx-auto scale-125 ">
+                <Avatar style={{height: 190, width: 190, marginTop:50}}
+                        src={profilePicPreview}
+                        alt={`...Controller.stringAvatar(username)`}
                 />
-                <p>Username: {username}</p>
-                <p>Email: {email}</p>
-                <label
-                    htmlFor="contained-button-file"
-                    className="mx-auto scale-125 "
-                >
-                    <Input
-                        accept="image/*"
-                        id="contained-button-file"
-                        type="file"
-                        onChange={(event) => {
-                            handleProfilePictureUpload(event);
-                        }}
-                    />
-                    <Button
-                        variant="contained"
-                        component="span"
-                        startIcon={<InsertPhotoIcon />}
-                    >
-                        Bild hinzufügen
-                    </Button>
-                </label>
+                <Input
+                    accept="image/*"
+                    id="contained-button-file"
+                    type="file"
+                    onChange={(event) => {
+                        handleProfilePictureUpload(event);
+                    }}/>
+            </label>
+            <div className="m-10 text-2xl">
+                <p><b>User name:</b> {username}</p>
+                <p><b>Email: </b>{email}</p>
+            </div>
+            <Button
+                startIcon={<SaveIcon/>}>
+                Speichern
+            </Button>
         </div>
     );
 };
