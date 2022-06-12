ef4d1d4e274cfd2c6a3dc42203ab3951d600078c
Eni Veshi <e.veshi@oth-aw.de>
2022-06-12 22:33:51 +0200

    add test login register
    Former-commit-id: 61502b39ce513fd06942ed834f5af05f0729eb1d

--- sys-src/client/src/components/Authentication/Login.test.tsx
+++ sys-src/client/src/components/Authentication/Login.test.tsx
@@ -1,8 +1,32 @@
 import { render } from '../../utils/test-utils';
+import { fireEvent } from '@testing-library/react';
 import Login from './Login';
 
-test('renders learn react link', () => {
-    const { getByText } = render(<Login />);
+const setup = () => {
+    const utils = render(<Login />);
+    const emailInput = utils.getByLabelText(
+        'Email / Username'
+    ) as HTMLInputElement;
+    const passwordInput = utils.getByLabelText('Password') as HTMLInputElement;
+    const submitButton = utils.getByTestId('login-button');
+    return {
+        emailInput,
+        passwordInput,
+        submitButton,
+        ...utils,
+    };
+};
 
+test('renders login component', () => {
+    const { getByText } = render(<Login />);
     expect(getByText(/login/i, { selector: 'h1' })).toBeInTheDocument();
+});
+
+test('It should change the email input', () => {
+    const { emailInput, passwordInput, submitButton } = setup();
+    fireEvent.change(emailInput, { target: { value: 'test@test.com' } });
+    expect(emailInput.value).toBe('test@test.com');
+
+    fireEvent.change(passwordInput, { target: { value: 'password1234' } });
+    expect(passwordInput.value).toBe('password1234');
 });
