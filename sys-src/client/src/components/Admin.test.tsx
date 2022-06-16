b907d28562833ff32b64edc70867e0a6887a0d24
Arsen Oganisyan <a.oganisyan@oth-aw.de>
2022-06-16 17:17:33 +0200

    Added some test, fixed movieRouter
    Former-commit-id: 09130c0bffcc70105e6d4d92769979ed96d9c218

--- sys-src/client/src/components/Admin.test.tsx
+++ sys-src/client/src/components/Admin.test.tsx
@@ -1,30 +1,44 @@
 import Admin from './Admin';
-import { render, screen } from '../utils/test-utils';
+import { fireEvent, render, screen } from '../utils/test-utils';
+import { debug } from 'console';
 
-test('Admin component test. Tests if all necessary inputs/buttons are present.', () => {
-    const { getByText } = render(<Admin />);
+describe('Test Admin component', () => {
+    test('Tests if all necessary inputs/buttons (UI) are present.', () => {
+        const { getByTestId } = render(<Admin />);
 
-    const heading = getByText(/film hinzufügen:/i, { selector: 'h1' });
-    expect(heading).toBeInTheDocument();
+        const heading = getByTestId('heading');
+        expect(heading).toBeInTheDocument();
 
-    const titleInput = screen.getByRole('titleInput');
-    expect(titleInput).toHaveTextContent('Titel');
+        const titleInput = getByTestId('titleInput');
+        expect(titleInput).toHaveTextContent('Titel');
 
-    const descriptionInput = screen.getByRole('descriptionInput');
-    expect(descriptionInput).toHaveTextContent('Beschreibung');
+        const descriptionInput = getByTestId('descriptionInput');
+        expect(descriptionInput).toHaveTextContent('Beschreibung');
 
-    const dateInput = screen.getByRole('datePicker');
-    expect(dateInput).toHaveTextContent('Erscheinungsdatum');
+        const dateInput = getByTestId('datePicker');
+        expect(dateInput).toHaveTextContent('Erscheinungsdatum');
 
-    const trailerInput = screen.getByRole('linkInput');
-    expect(trailerInput).toHaveTextContent('Trailer-Link');
+        const trailerInput = getByTestId('linkInput');
+        expect(trailerInput).toHaveTextContent('Trailer-Link');
 
-    const addImgBtn = screen.getByText(/bild hinzufügen/i);
-    expect(addImgBtn).toBeInTheDocument();
+        const addImgBtn = getByTestId('addImgButton');
+        expect(addImgBtn).toBeInTheDocument();
 
-    const addMovieBtn = screen.getByRole('addMovieBtn');
-    expect(addMovieBtn).toBeInTheDocument();
+        const addMovieBtn = getByTestId('addMovieBtn');
+        expect(addMovieBtn).toBeInTheDocument();
 
-    const cancelBtn = screen.getByText(/abbrechen/i);
-    expect(cancelBtn).toBeInTheDocument();
+        const cancelBtn = getByTestId('cancelBtn');
+        expect(cancelBtn).toBeInTheDocument();
+    });
+
+    test('Tests validation.', () => {
+        const { getByTestId } = render(<Admin />);
+
+        const addImgBtn = getByTestId('addImgButton');
+        const titleInput = getByTestId('titleInput');
+
+        fireEvent.click(addImgBtn);
+
+        expect(titleInput).toHaveClass('Mui-error');
+    });
 });
