5642778b8ca7022fa56c384b4687b94703e7ab33
Eni Veshi <e.veshi@oth-aw.de>
2022-06-19 23:04:24 +0200

    removed test call an backend
    Former-commit-id: 202fb6bd6248a3c17ef97708371a6aac264a2817

--- sys-src/client/src/components/Authentication/Login.test.tsx
+++ sys-src/client/src/components/Authentication/Login.test.tsx
@@ -1,7 +1,14 @@
 import { render } from '../../utils/test-utils';
 import { fireEvent } from '@testing-library/react';
 import Login from './Login';
-import Backend from '../../api/Backend';
+import axios from 'axios';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+mockedAxios.post.mockResolvedValue({
+    data: { token: 'token-received' },
+});
 
 const setup = () => {
     const utils = render(<Login />);
@@ -18,35 +25,19 @@ const setup = () => {
     };
 };
 
-test('renders login component', () => {
-    const { getByText } = render(<Login />);
-    expect(getByText(/login/i, { selector: 'h1' })).toBeInTheDocument();
-});
+describe('login', () => {
+    afterEach(jest.clearAllMocks);
+    test('It should change the email, password input ans success login', async () => {
+        const { emailInput, passwordInput, submitButton } = setup();
 
-test('It should change the email input', () => {
-    const { emailInput, passwordInput, submitButton } = setup();
-    fireEvent.change(emailInput, { target: { value: 'test@test.com' } });
-    expect(emailInput.value).toBe('test@test.com');
+        fireEvent.change(emailInput, { target: { value: 'test@test.com' } });
+        expect(emailInput.value).toBe('test@test.com');
 
-    fireEvent.change(passwordInput, { target: { value: 'password1234' } });
-    expect(passwordInput.value).toBe('password1234');
-});
+        fireEvent.change(passwordInput, { target: { value: 'password1234' } });
+        expect(passwordInput.value).toBe('password1234');
 
-test('make fake login', async () => {
-    try {
-        await Backend.login({
-            identifier: 'test',
-            password: 'test',
-        });
-    } catch (e: any) {
-        expect(e.message).toBe('Request failed with status code 400');
-    }
-});
+        fireEvent.click(submitButton);
 
-test('make admin login', async () => {
-    await Backend.login({
-        identifier: 'admin',
-        password: '!TjNL7(hmadc74~&',
+        expect(mockedAxios.post).toHaveBeenCalledTimes(1);
     });
-    expect(localStorage.getItem('token')).toBeDefined();
 });
